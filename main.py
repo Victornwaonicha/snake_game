cc4824332d3b3f87913f2cac54bc39380c6dd2d9
Victor <victornwaonicha@gmail.com>
2025-07-16 20:17:54 -0700

    Completed_segment

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from scoreboard import Scoreboard
 import time
 
 screen = Screen()
-screen.setup(width=450, height=600)
+screen.setup(width=600, height=600)
 screen.bgcolor("cyan")
 screen.title("The Snake Game")
 screen.tracer(0)
@@ -30,7 +30,20 @@ while game_is_on:
     #Detect collision with food.
     if snake.head.distance(food) < 15:
         food.refresh()
+        snake.extend()
         scoreboard.increase_score()
+
+    #Detect collision with wall.
+    if snake.head.xcor() > 280 or snake.head.xcor() < - 280 or snake.head.ycor() > 280 or snake.head.ycor() < - 280:
+        game_is_on = False
+        scoreboard.game_over()
+
+    #Detect collision with tail
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
+
 
 
 
