d7f3e5220a8228206f331857db5e8c248e0dc6c3
Victor <victornwaonicha@gmail.com>
2025-07-29 18:14:50 -0700

    Completed

--- scoreboard.py
+++ scoreboard.py
@@ -7,23 +7,30 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.color("black")
+        self.high_score = 0
+        self.color("red")
         self.penup()
         self.goto(0, 277)
         self.hideturtle()
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
+        self.update_scoreboard()
+
+    # def game_over(self):
+    #     self.goto(0, 0)
+    #     self.write("GAME OVER", align=ALIGNMENT, font=FONT)
 
 
     def increase_score(self):
         self.score += 1
-        self.clear()
         self.update_scoreboard()
 
 
